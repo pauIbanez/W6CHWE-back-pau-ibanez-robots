6f286a1622a62395b1abedba3c39249666638154
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-18 18:47:28 +0100

    added database connection handler

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 require("dotenv").config();
 const debug = require("debug")("app:server");
-
 const chalk = require("chalk");
+
+const connectToDB = require("./database");
 const startServer = require("./server");
 
 const port = process.env.PORT || 8080;
+const connectionString = process.env.CONN_STRING || "";
 
 (async () => {
   try {
     await startServer(port);
+    await connectToDB(connectionString);
   } catch (error) {
     debug(chalk.redBright(`Error while setting up server: ${error.message}`));
   }

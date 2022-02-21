e9cdef2ab86e9d2cc117e8e5d737b081d901cf9d
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-21 20:47:32 +0100

    added mail utils

---
+++ src/utils/mailer.js
@@ -0,0 +1,36 @@
+const chalk = require("chalk");
+const nodemailer = require("nodemailer");
+const debug = require("debug")("app:mailservice");
+
+const config = {
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+};
+
+const transporter = nodemailer.createTransport(config);
+
+const accountsMail = {
+  from: process.env.EMAIL,
+  subject: "Robots account activation",
+  text: "test text",
+};
+
+const sendEmail = (mail = {}) => {
+  const emailToSend = { ...accountsMail, ...mail };
+
+  transporter.sendMail(emailToSend, (error, info) => {
+    if (error) {
+      debug(
+        chalk.redBright(
+          `Error while sending activation email to ${emailToSend.to}`
+        )
+      );
+      return;
+    }
+    debug(chalk.yellowBright(`Activation email sent to ${emailToSend.to}`));
+    debug(chalk.yellowBright(`Email info:\n ${info}`));
+  });
+};
